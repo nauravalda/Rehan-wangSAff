14a11d273eb1bd32265bcf4cf7da7002a86d1b48
nauravalda <16521058@mahasiswa.itb.ac.id>
2024-04-22 18:44:13 +0700

    add isprime function

--- RSA.py
+++ RSA.py
@@ -1,8 +1,19 @@
 # generate key pair
 import random
-from sympy import isprime
 import base64
 import struct
+
+def isprime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
 def generate_prime_number():
     while True:
